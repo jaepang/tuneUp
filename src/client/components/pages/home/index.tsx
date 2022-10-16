d188b25a125165a62b902f39ce4c5118398e88a8
jaepang <jake_king@kakao.com>
2022-10-16 11:59:45 +0900

    fix hydration error

--- src/client/components/pages/home/index.tsx
+++ src/client/components/pages/home/index.tsx
@@ -8,18 +8,18 @@ import { useQuery } from 'react-query'
 import { requestsQuery } from '@client/shared/queries'
 
 export default function HomePageComponent() {
-  const { isLoggedIn } = useAccount()
+  const { isLoggedIn, isMeLoading } = useAccount()
   const { data, isLoading } = useQuery(['homeFeed'], () => requestsQuery({ skip: 0, take: 10 }))
   const { width } = useWindowSize()
   const isTablet = width <= 1024
 
   const rowStyle = {
-    marginTop: isTablet ? '64px' : '130px',
+    marginTop: isTablet ? '84px' : '130px',
   }
 
   return (
     <Row style={rowStyle}>
-      {!isLoggedIn && <MainBanner />}
+      {!isMeLoading && !isLoggedIn && <MainBanner />}
       <h1>지금 공연하고 싶은 동아리</h1>
       {!isLoading && <CardFeed {...{ data: data?.requestFeed }} />}
     </Row>
